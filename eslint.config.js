edba42780a3098900204b6e4481298101851992a
DiegoQuintanaF <diegoquinfa@gmail.com>
2025-02-06 23:34:27 -0500

    chore: configured eslint and prettier for ts

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,17 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
 import neostandard from 'neostandard'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  {
+    files: ['src/**/*.{js,mjs,cjs,ts}']
+  },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   ...neostandard(),
   eslintConfigPrettier
 ]
