59087bf67ea6f743ec64c9a68e954285f9accb03
Diego Quintana <diegoquinfa@gmail.com>
2025-02-09 17:44:10 -0500

    refactor: translate to english

--- src/api/tasks/domain/Task.ts
+++ src/api/tasks/domain/Task.ts
@@ -1,32 +1,13 @@
-// import { z } from 'zod'
-//
-// export const TaskSchema = z.object({
-//   title: z.string().min(1),
-//   description: z.string().min(1),
-//   repeat: z
-//     .union([
-//       z.literal('anytime', {
-//         message: 'cualquier momento',
-//         description: 'error momento'
-//       }),
-//       z.literal('daily'),
-//       z.literal('weekly'),
-//       z.literal('yearly')
-//     ])
-//     .default('anytime')
-// })
-//
-// export type Task = z.infer<typeof TaskSchema>
 import { z } from 'zod'
 
 export const TaskSchema = z.object({
-  title: z.string().min(1, { message: 'El título es obligatorio.' }),
-  description: z.string().min(1, { message: 'La descripción es obligatoria.' }),
+  title: z.string().min(1, { message: 'The title is required.' }),
+  description: z.string().min(1, { message: 'The description is required.' }),
   repeat: z
     .enum(['anytime', 'daily', 'weekly', 'yearly'], {
       errorMap: () => ({
         message:
-          'El valor de "repeat" debe ser: anytime, daily, weekly o yearly.'
+          'The value of "repeat" must be: anytime, daily, weekly, or yearly.'
       })
     })
     .default('anytime')
